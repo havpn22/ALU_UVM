11f6e09d9006b4accb68b13f927fc6eae3d91864
havpn22 <havalapn22@gmail.com>
2025-09-19 12:34:24 +0530

    Create alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,15 @@
+`include "uvm_macros.svh"
+
+package alu_pkg;
+        import uvm_pkg ::*;
+        `include "sequence_item.sv"
+        `include "sequence.sv"
+        `include "sequencer.sv"
+        `include "driver.sv"
+        `include "monitor.sv"
+        `include "agent.sv"
+        `include "scoreboard.sv"
+        `include "coverage.sv"
+        `include "environment.sv"
+        `include "test.sv"
+endpackage
